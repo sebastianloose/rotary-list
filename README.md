01aa37e3bcbcec799066e7ceccd78339b88a9969
Sebastian Loose <sebastian.loose@yahoo.de>
2022-07-07 12:38:22 +0200

    Minor

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ To use this plugin, the `minSdkVersion` of your android project has to be at lea
       flutter:
         sdk: flutter
 
-      # other plugins ...
+      # other dependencies ...
 
       rotary_list:
         git:
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,21 +10,6 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> elements = [];
-    for (int i = 0; i < 100; i++) {
-      elements.add(
-        Container(
-          padding: const EdgeInsets.symmetric(vertical: 10),
-          color: i % 2 == 0 ? Colors.blue : Colors.lightBlue,
-          child: Text(
-            i.toString(),
-            style: const TextStyle(color: Colors.white),
-            textAlign: TextAlign.center,
-          ),
-        ),
-      );
-    }
-
     return MaterialApp(
       home: Scaffold(
         body: Center(
